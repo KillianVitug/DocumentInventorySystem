c7ac7ff7605a18715c766ae334115b4e29d3b065
Kyle Vitug <kyle_vitug@yahoo.com.ph>
2024-10-23 15:34:21 +0800

    changes

--- client/src/pages/Company/routes.jsx
+++ client/src/pages/Company/routes.jsx
@@ -1,6 +1,6 @@
 import { Route } from 'react-router-dom';
 import BirPermitTypesCrudPage from './BirPermitsTypesCrudPage';
-import BirPermitsCrudPage from './BirPermitsCrudPage-old';
+import BirPermitsCrudPage from './BirPermitsCrudPage';
 import PublicRoute from '../../components/PublicRoute';
 import ProtectedRoute from '../../components/ProtectedRoute';
 
