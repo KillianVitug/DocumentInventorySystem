82c8dcb4ad6990a2283b5fafe2517301c5bce268
Kyle Vitug <kyle_vitug@yahoo.com.ph>
2024-10-29 23:39:13 +0800

    save

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,22 +12,58 @@ export default function NavBar() {
   }
 
   return (
-    <Navbar bg="light" expand="lg" className="px-4 py-3 border-bottom border-dark">
-      <Navbar.Brand as={Link} to="/">Storesystem</Navbar.Brand>
+    <Navbar
+      bg="light"
+      expand="lg"
+      className="px-4 py-3 border-bottom border-dark"
+    >
+      <Navbar.Brand as={Link} to="/">
+        Storesystem
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+          <Nav.Link as={Link} to="/dashboard">
+            Dashboard
+          </Nav.Link>
           <NavDropdown title="Company" id="company-dropdown">
-            <NavDropdown.Item as={Link} to="/bir-permit-types">BIR Permit Types</NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/bir-permits">BIR Permits</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/bir-permit-types">
+              BIR Permit Types
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/bir-permits">
+              BIR Permits
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Transactions" id="company-dropdown">
+            <NavDropdown.Item as={Link} to="/purchases">
+              Purchases
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/bir-permits">
+              Inventory Adjustments
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Company" id="company-dropdown">
+            <NavDropdown.Item as={Link} to="/bir-permit-types">
+              BIR Permit Types
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/bir-permits">
+              BIR Permits
+            </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Documents" id="company-dropdown">
-            <NavDropdown.Item as={Link} to="/documents">View Company</NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/documents/add">Add Company</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/documents">
+              View Company
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/documents/add">
+              Add Company
+            </NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link as={Link} to="/register">Add User</Nav.Link>
-          <Nav.Link as={Link} to="/delivery">Delivery</Nav.Link>
+          <Nav.Link as={Link} to="/register">
+            Add User
+          </Nav.Link>
+          <Nav.Link as={Link} to="/delivery">
+            Delivery
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
